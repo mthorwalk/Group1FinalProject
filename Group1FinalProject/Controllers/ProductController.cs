74da96462f9d059bfaf1133719316ad0a77e413c
mwalker39 <mwalker39@huskers.unl.edu>
2022-04-10 23:50:41 -0500

    Skeleton Product MVC

---
+++ Group1FinalProject/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Group1FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group1FinalProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static IList<Product> products = new List<Product>
+        {
+            new Product() {ProductId = 1, ProductName = "Test1"},
+            new Product() {ProductId = 2, ProductName = "Test2"}
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
---
+++ Group1FinalProject/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Group1FinalProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
