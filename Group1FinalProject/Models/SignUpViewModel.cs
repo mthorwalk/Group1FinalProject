37e0be7eaf6ad416cf2e9a74d86a107e47bd0a81
uismail <69741155+uismail@users.noreply.github.com>
2022-04-17 13:47:24 -0500

    Add files via upload

---
+++ Group1FinalProject/Models/SignUpViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Group1FinalProject.Models
+{
+    public class SignUpViewModel
+    {
+        public int? Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        public string? Password { get; set; }
+        public Boolean? Success { get; set; }
+        public string? ErrorMessage { get; set; }
+
+
+    }
+}
