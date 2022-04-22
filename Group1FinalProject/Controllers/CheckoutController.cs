e1ed249fd219895535b190e2a2090510e7ee6ef5
mwalker39 <mwalker39@huskers.unl.edu>
2022-04-22 13:04:34 -0500

    Skeleton Checkout + Unit Test Project

---
+++ Group1FinalProject/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group1FinalProject.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
