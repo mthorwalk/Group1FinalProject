213fc3c4ba5d4d7d71dd72dd84e0829bc10b107d
vnguyen38 <vnguyen38@huskers.unl.edu>
2022-04-22 19:35:05 -0500

    fixed home/cart pages

--- Group1FinalProject/Models/CartItem.cs
+++ Group1FinalProject/Models/CartItem.cs
@@ -5,11 +5,19 @@ namespace Group1FinalProject.Models
     public class CartItem : Product
     {
         public int quantity = 1;
+        public double productsPrice;
 
         public int Quantity
         {
             get { return quantity; }
             set { quantity = value; }
+        }
+
+        [System.ComponentModel.DataAnnotations.DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)] //prints prices with two decimal places
+        public double ProductsPrice
+        {
+            get { return CalculateProductsPrice(); }
+            set { productsPrice = value; }
         }
 
         public double CalculateProductsPrice()
