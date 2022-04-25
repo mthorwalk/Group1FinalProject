fb3d706f97c2ce8be4c43706487bf2d8f22402c4
mwalker39 <mwalker39@huskers.unl.edu>
2022-04-25 01:17:52 -0500

    Unit Tests

---
+++ Group1FinalProjectUnitTests/AccountControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group1FinalProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Group1FinalProjectUnitTests
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void AccountIndexView()
+        {
+            var controller = new AccountController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ViewName);
+        }
+        [TestMethod]
+        public void AccountSignUpView()
+        {
+            var controller = new AccountController();
+
+            var result = controller.SignUp() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("SignUp", result.ViewName);
+        }
+    }
+}
---
+++ Group1FinalProjectUnitTests/CartControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group1FinalProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Group1FinalProjectUnitTests
+{
+    [TestClass]
+    public class CartControllerTest
+    {
+        [TestMethod]
+        public void CartIndexView()
+        {
+            var controller = new CartController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
