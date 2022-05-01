8bf1672c83fcba64723578fbbd2f749574bdf6e3
uismail <69741155+uismail@users.noreply.github.com>
2022-05-01 17:08:06 -0500

    Update SignInViewModel.cs

--- Group1FinalProject/Models/SignInViewModel.cs
+++ Group1FinalProject/Models/SignInViewModel.cs
@@ -1,12 +1,21 @@
-﻿namespace Group1FinalProject.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace Group1FinalProject.Models
 {
     public class SignInViewModel
     {
         public int? Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email isn't in the correct format")]
         public string? Email { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength = 8, ErrorMessage = "Password should be at least 8 characters")]
         public string? Password { get; set; }
+      
         public Boolean? Success { get; set; }
         public string? ErrorMessage { get; set; }
 
     }
 }
+
