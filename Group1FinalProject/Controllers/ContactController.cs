a6a25a6d2077cc72f429277f147e297965a7b75b
tunhatthanhthao <69448536+tunhatthanhthao@users.noreply.github.com>
2022-05-05 22:45:37 -0500

    Add files via upload

---
+++ Group1FinalProject/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group1FinalProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+    }
+}
